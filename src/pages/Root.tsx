e8df500050abf4ece0ec8c3c19bfb189060b0aa1
mozart <mozartgranado08@gmail.com>
2026-03-23 14:03:02 +0800

    implement adding of session

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import SessionContextProvider from '../store/sessions-context';
 
 export default function Root() {
   return (
-    <>
+    <SessionContextProvider>
       <Header />
       <Outlet />
-    </>
+    </SessionContextProvider>
   );
 }
